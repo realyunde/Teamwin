735fc4e4df97c9f45c9266a5ef99ae64e66746bf
Yunde Liu <yundepub@outlook.com>
2021-03-22 10:58:11 +0800

    change timezone and database config

--- scrum/settings.py
+++ scrum/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'scrum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
